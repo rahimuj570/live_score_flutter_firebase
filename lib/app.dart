0f3a06f965605da7a956da92579be43aab5318c5
rahimuj570 <rahimujjaman2002@gmail.com>
2025-11-01 22:39:26 +0600

    stay loged in untill logout functionality added

--- lib/app.dart
+++ lib/app.dart
@@ -1,6 +1,7 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:live_score_app2/home_screen.dart';
 import 'package:live_score_app2/login_screen.dart';
-import 'package:live_score_app2/signup_screen.dart';
 
 class LiveScoreApp extends StatefulWidget {
   const LiveScoreApp({super.key});
@@ -12,6 +13,16 @@ class LiveScoreApp extends StatefulWidget {
 class _LiveScoreAppState extends State<LiveScoreApp> {
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(home: LoginScreen());
+    return MaterialApp(
+      home: StreamBuilder(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (context, asyncSnapshot) {
+          if (asyncSnapshot.hasData) {
+            return HomeScreen();
+          }
+          return LoginScreen();
+        },
+      ),
+    );
   }
 }
